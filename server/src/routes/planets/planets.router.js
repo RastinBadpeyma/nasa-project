97b47395ec139d5df3166b42db9ef249e6e451fe
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2023-12-26 22:19:34 +0330

    controller and model

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -2,6 +2,9 @@ const express  = require('express');
 
 const planetsRouter = express.Router();
 
+const {
+   getAllPlanets
+} = require('./planets/planetsRouter');
 
 planetsRouter:get('/planets' , getAllPlanets);
 
