6900205c9d5b3b4f65deacff846bebf0751ab667
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-01-01 19:41:04 +0330

    promise and await

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const planets= require('../../models/planets.mode');
+const {planets} = require('../../models/planets.mode');
 
 function getAllPlanets(req , res){
    return res.status(200).json(planets)
--- server/src/server.js
+++ server/src/server.js
@@ -7,8 +7,16 @@ const app = require('./app');
 
 const PORT = 8000;
 
+const {loadPlanetsData} =  require('./models/planets.mode')
+
 const Server = http.createServer(app);
 
-Server.listen(PORT , ()=>{
- console.log(`${PORT}}`);
-});
+async function startServer() {
+
+   await loadPlanetsData();
+   Server.listen(PORT , ()=>{
+      console.log(`${PORT}}`);
+     });
+}
+
+startServer();
