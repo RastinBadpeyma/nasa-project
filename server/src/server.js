76f60958c04182675cfe4bd24d99969f20b9a9e2
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-02-16 14:13:59 +0330

    save 8 habitable planets in  mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+const mongoose = require('mongoose');
 const express = require('express');
 
 // const app = express();
@@ -7,16 +8,25 @@ const app = require('./app');
 
 const PORT = 8000;
 
+const MONGO_URL = "mongodb://localhost:27017/nasa"
 const {loadPlanetsData} =  require('./models/planets.mode')
 
 const Server = http.createServer(app);
 
-async function startServer() {
+mongoose.connection.once('open' , () =>{
+console.log('MongoDB connection ready!');
+});
 
+mongoose.connection.on('error' ,(err) => {
+ console.error(err);
+});
+
+async function startServer() {
+    await mongoose.connect(MONGO_URL);
    await loadPlanetsData();
    Server.listen(PORT , ()=>{
       console.log(`${PORT}}`);
      });
-}
+ }
 
 startServer();
